397c49f595061a43a91036ebc0714ed9268e3157
Omkar <omkar.mahajan872@gmail.com>
2024-02-20 11:47:51 +0530

    added ask questions to gemini api

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/userController")
 const customHabitController = require("../controllers/customHabitController")
 const defaultHabitController = require("../controllers/defaultHabitController")
+const geminiController = require("../controllers/geminiController")
 
 //user Routes
 router.post("/users/addUser", userController.addUser);
@@ -15,6 +16,9 @@ router.post("/customHabit/addCustomHabit", customHabitController.addCustomHabit)
 //default habit
 router.post("/defaultHabit/addDefaultHabit", defaultHabitController.addDefaultHabit);
 router.get("/defaultHabit/getAllDefaultHabits", defaultHabitController.getAllDefaultHabits);
+
+//gemini routes
+router.post("/gemini/askQuestion", geminiController.askQuestion);
 
 
 module.exports = router;
