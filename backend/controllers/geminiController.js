44f2c48b3d1d947ea454a873b3ae0ee252198b71
Omkar <omkar.mahajan872@gmail.com>
2024-02-20 12:15:29 +0530

    refactored gemini code for global use

--- backend/controllers/geminiController.js
+++ backend/controllers/geminiController.js
@@ -1,31 +1,10 @@
-const { asyncErrorHandler, HttpError } = require("../models/Model")
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// 1. Configuration
-const api_key = 'AIzaSyADJNXPFcAxBiYSCMyCIyb534isnzBWYu0';
-const genAI = new GoogleGenerativeAI(api_key);
-const generationConfig = { temperature: 0.9, topP: 1, topK: 1, maxOutputTokens: 4096 };
-
-// 2. Initialise Model
-const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
+const { asyncErrorHandler, HttpError, askGemini } = require("../models/Model")
 
 
 const askQuestion = asyncErrorHandler(async (req, res, next) => {
     const { question } = req.body;
 
-    async function generateContent(question) {
-        try {
-            const prompt = question
-            const result = await model.generateContent(prompt);
-            const response = await result.response;
-            const text = response.text();
-            return text
-        } catch (error) {
-            console.error('Error generating content:', error);
-        }
-    }
-
-    const result = await generateContent(question)
+    const result = await askGemini(question)
     if (!result) {
         throw new HttpError("Something went wrong", 500)
     }
--- backend/models/Model.js
+++ backend/models/Model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const nodemailer = require('nodemailer')
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -44,9 +45,30 @@ const asyncErrorHandler = (func) => {
 }
 
 
+// 1. Configuration
+const api_key = 'AIzaSyADJNXPFcAxBiYSCMyCIyb534isnzBWYu0';
+const genAI = new GoogleGenerativeAI(api_key);
+const generationConfig = { temperature: 0.9, topP: 1, topK: 1, maxOutputTokens: 4096 };
+
+// 2. Initialise Model
+const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
+
+async function askGemini(question) {
+    try {
+        const prompt = question
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        return text
+    } catch (error) {
+        console.error('Error generating content:', error);
+    }
+}
+
 
 exports.User = User
 exports.customHabit = customHabit
 exports.defaultHabit = defaultHabit
+exports.askGemini = askGemini
 exports.HttpError = HttpError
 exports.asyncErrorHandler = asyncErrorHandler
